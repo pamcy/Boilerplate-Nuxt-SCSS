f3190268837a304879042b34452cf6503434a2f2
Chinyi Pam <chinyi.pam@gmail.com>
2023-11-10 10:40:14 +0800

    add Nuxt layout and demo pages

---
+++ pages/About/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>About me page</h1>
+    <p>Hallo Welt</p>
+    <img src="/images/me.png" alt="" />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Homepage</h1>
+    <img src="/images/banner-1-desktop.jpg" alt="" style="width: 100%" />
+  </div>
+</template>
