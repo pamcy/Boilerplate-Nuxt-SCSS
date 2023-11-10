f54c4a1e5e8015fad6ad73c29c9c7f9561ade535
Chinyi Pam <chinyi.pam@gmail.com>
2023-11-10 11:43:20 +0800

    add default scss files

--- pages/About/index.vue
+++ pages/About/index.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div>
+  <div class="about">
     <h1>About me page</h1>
     <p>Hallo Welt</p>
     <img src="/images/me.png" alt="" />
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,11 @@ console.log('good to go!')
 <template>
   <div>
     <h1>Homepage</h1>
+    <ul>
+      <li>Captain</li>
+      <li>Ironman</li>
+    </ul>
+    <button type="button" class="btn btn-primary">Click me</button>
     <img src="/images/banner-1-desktop.jpg" alt="" style="width: 100%" />
   </div>
 </template>
